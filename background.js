2883a53e52090d944744fbe55a872bf9b418a008
B-Ashdown <ben99944@gmail.com>
2023-06-14 20:24:27 +1000

    Further progress

--- background.js
+++ background.js
@@ -1,6 +1,13 @@
-chrome.runtime.onMessage.addListener(callback);
 
-function callback(message, sender, sendResponse) {
-    const tab = sender.tab.id
-    chrome.tabs.sendMessage(tab, message = "hello2")
+chrome.webNavigation.onCompleted.addListener(callback)
+chrome.webNavigation.onHistoryStateUpdated.addListener(callback)
+
+
+function callback(details) {
+    if (details.url.includes("youtube.com/watch?v=")) {
+        const tab = details.tabId
+        chrome.tabs.sendMessage(tab, message = "hello2")
+    }
+    chrome.scripting.executeScript({})
+    console.log("HIIIIIIIIIIIIIIII " + details.url)
 }
--- content-script.js
+++ content-script.js
@@ -1,8 +1,7 @@
-const info = document.getElementById("info")
-const views = info.children[0]
-
-chrome.runtime.sendMessage(message = "hello")
 chrome.runtime.onMessage.addListener(() => {
     window.alert("HELLO FROM content")
+    const info = document.getElementById("info")
+    const views = info.children[0]
+    console.log(views)
   });
 
